8e723ba4bfb3bca8b0c8df0fcbe99b7441332c52
Jesus David Quispe Quispe <jumbo7u7@gmail.com>
2025-05-23 22:27:38 -0500

    Add LoginController and implement all Course API features

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
@@ -34,6 +35,10 @@ Route::apiResource('v1/posts', PostV1::class)->only('index','show', 'destroy')
 //Route V2 
 Route::apiResource('v2/posts', PostV2::class)->only(['index','show', 'destroy'])
     ->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class,
+
+'login']);
     
 
 
